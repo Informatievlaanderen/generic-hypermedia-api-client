6c98639da0435c3c9805f9f065ea48f6b0ee9835
Dwight Van Lancker <Dwight.VanLancker@UGent.be>
2018-08-22 09:51:43 +0200

    Index file changed

--- index.ts
+++ index.ts
@@ -9,16 +9,6 @@ import {FullTextSearchHandler} from "./FullTextSearchHandler";
 
 try {
     //TODO : add your code snippet here
-    const client = new ApiClient(null);
-    const fullTextSearch = new FullTextSearchHandler({
-        callback: (callback) => {},
-        apiClient: client,
-        queryValues: ['key']
-    })
-
-    client.fetch('http://localhost:3001/api/personen', [fullTextSearch]);
-
-
 } catch(e){
     console.log(e);
 }
