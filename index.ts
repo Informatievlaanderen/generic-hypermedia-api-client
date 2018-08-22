7f813c6fa5a60afe32ed8eb7a44f0eb8b4ba4ca5
Dwight Van Lancker <Dwight.VanLancker@UGent.be>
2018-08-22 11:14:08 +0200

    index.ts made empty

--- index.ts
+++ index.ts
@@ -9,15 +9,7 @@ import {FullTextSearchHandler} from "./lib/FullTextSearchHandler";
 
 try {
     //TODO : add your code snippet here
-    const client = new ApiClient(null);
-    const test = new MetadataApiHandler({
-        metadataCallback: (callback) => {console.log(callback)},
-        apiClient: client,
-        followDocumentationLink: false,
-        subjectStream: client.subjectStream
-    });
-
-    client.fetch('http://localhost:3001/api', [test]);
+    
 } catch(e){
     console.log(e);
 }
