592c68b7696f6f260ae47634f46c3dab22d3fe8d
Dwight Van Lancker <Dwight.VanLancker@UGent.be>
2018-08-29 14:54:33 +0200

    Add script in /bin for easy testing

---
+++ bin/generic-hypermedia-api-client.js
@@ -0,0 +1,93 @@
+"use strict";
+exports.__esModule = true;
+var MetadataHandler_1 = require("../lib/MetadataHandler");
+var ApiClient_1 = require("../lib/ApiClient");
+var PaginationHandler_1 = require("../lib/PaginationHandler");
+var LanguageHandler_1 = require("../lib/LanguageHandler");
+var VersioningHandler_1 = require("../lib/VersioningHandler");
+var FullTextSearchHandler_1 = require("../lib/FullTextSearchHandler");
+var CRUDHandler_1 = require("../lib/CRUDHandler");
+var minimist = require('minimist');
+process.argv.splice(0, 2);
+var args = minimist(process.argv);
+var handlers = [];
+if (args._.length < 2 || args._.length > 7 || args.h || args.help) {
+    // Print command usage
+    process.stderr.write("\n    generic-hypermedia-api-client requires an URL and one or more handlers\n    \n    Usage:    \n    generic-hypermedia-api-client http://example.org handler1 handler2\n    \n    Handlers:\n        * metadata\n        * pagination\n        * language\n        * versioning\n        * full_text_search\n        * crud\n    \n    Options:\n        --followdoclink         follow the documentation link in the MetadataHandler\n        --followversionlink     follow the versioned URL in the VersionHandler\n        --help                  print this help message\n");
+    process.exit(1);
+}
+function createHandlers(client) {
+    var handlers = [];
+    for (var i = 1; i < args._.length; i++) {
+        switch (args._[i]) {
+            case 'metadata':
+                var followDocLink = false;
+                if (args.followdoclink) {
+                    followDocLink = true;
+                }
+                handlers.push(new MetadataHandler_1.MetadataHandler({
+                    metadataCallback: function (metadata) { return console.log(metadata); },
+                    apiClient: client,
+                    followDocumentationLink: followDocLink
+                }));
+                break;
+            case 'pagination':
+                handlers.push(new PaginationHandler_1.PaginationHandler({
+                    pagedataCallback: function (pagedata) { return console.log(pagedata); },
+                    subjectStream: client.subjectStream
+                }));
+                break;
+            case 'language':
+                handlers.push(new LanguageHandler_1.LanguageHandler({
+                    languageCallback: function (languagedata) {
+                        languagedata.stream.on('data', function (data) {
+                            console.log(data);
+                        });
+                    },
+                    acceptLanguageHeader: 'en'
+                }));
+                break;
+            case 'versioning':
+                var followVersionLink = false;
+                if (args.followversionlink) {
+                    followVersionLink = true;
+                }
+                handlers.push(new VersioningHandler_1.VersioningHandler({
+                    versionCallback: function (versiondata) {
+                        versiondata.stream.on('data', function (data) {
+                            console.log(data);
+                        });
+                    },
+                    apiClient: client,
+                    datetime: new Date(),
+                    followLink: followVersionLink
+                }));
+                break;
+            case 'full_text_search':
+                handlers.push(new FullTextSearchHandler_1.FullTextSearchHandler({
+                    callback: function (ftsdata) {
+                        ftsdata.stream.on('data', function (data) {
+                            console.log(data);
+                        });
+                    },
+                    apiClient: client,
+                    fetchQueryURL: false
+                }));
+                break;
+            case 'crud':
+                handlers.push(new CRUDHandler_1.CRUDHandler({
+                    crudCallback: function (cruddata) {
+                        console.log(cruddata);
+                    }
+                }));
+        }
+    }
+    return handlers;
+}
+function processURL() {
+    var URL = args._[0];
+    var client = new ApiClient_1.ApiClient(null);
+    var handlers = createHandlers(client);
+    client.fetch(URL, handlers);
+}
+processURL();
---
+++ bin/generic-hypermedia-api-client.ts
@@ -0,0 +1,123 @@
+import {MetadataHandler} from "../lib/MetadataHandler";
+import {ApiClient} from "../lib/ApiClient";
+import {PaginationHandler} from "../lib/PaginationHandler";
+import {LanguageHandler} from "../lib/LanguageHandler";
+import {VersioningHandler} from "../lib/VersioningHandler";
+import {version} from "punycode";
+import {FullTextSearchHandler} from "../lib/FullTextSearchHandler";
+import {CRUDHandler} from "../lib/CRUDHandler";
+import {IApiHandler} from "../lib/IApiHandler";
+
+const minimist = require('minimist');
+
+process.argv.splice(0, 2);
+const args = minimist(process.argv);
+
+let handlers = [];
+
+if (args._.length < 2 || args._.length > 7 || args.h || args.help) {
+    // Print command usage
+    process.stderr.write(`
+    generic-hypermedia-api-client requires an URL and one or more handlers
+    
+    Usage:    
+    generic-hypermedia-api-client http://example.org handler1 handler2
+    
+    Handlers:
+        * metadata
+        * pagination
+        * language
+        * versioning
+        * full_text_search
+        * crud
+    
+    Options:
+        --followdoclink         follow the documentation link in the MetadataHandler
+        --followversionlink     follow the versioned URL in the VersionHandler
+        --help                  print this help message
+`);
+    process.exit(1);
+}
+
+function createHandlers(client: ApiClient): Array<IApiHandler> {
+    let handlers = [];
+    for(let i = 1 ; i < args._.length ; i++){
+        switch (args._[i]) {
+            case 'metadata':
+                let followDocLink = false;
+                if(args.followdoclink){
+                    followDocLink = true;
+                }
+                handlers.push(new MetadataHandler({
+                    metadataCallback: (metadata) => console.log(metadata),
+                    apiClient: client,
+                    followDocumentationLink: followDocLink
+                }));
+                break;
+
+            case 'pagination':
+                handlers.push(new PaginationHandler({
+                    pagedataCallback: (pagedata) => console.log(pagedata),
+                    subjectStream: client.subjectStream
+                }));
+                break;
+
+            case 'language':
+                handlers.push(new LanguageHandler({
+                    languageCallback: (languagedata) => {
+                        languagedata.stream.on('data', data => {
+                            console.log(data);
+                        });
+                    },
+                    acceptLanguageHeader: 'en'
+                }));
+                break;
+
+            case 'versioning':
+                let followVersionLink = false;
+                if(args.followversionlink){
+                    followVersionLink = true
+                }
+                handlers.push(new VersioningHandler({
+                    versionCallback: (versiondata) => {
+                        versiondata.stream.on('data', data => {
+                            console.log(data);
+                        });
+                    },
+                    apiClient: client,
+                    datetime: new Date(),
+                    followLink: followVersionLink
+                }));
+                break;
+
+            case 'full_text_search':
+                handlers.push(new FullTextSearchHandler({
+                    callback: (ftsdata) => {
+                        ftsdata.stream.on('data', data => {
+                            console.log(data);
+                        })
+                    },
+                    apiClient: client,
+                    fetchQueryURL: false
+                }));
+                break;
+
+            case 'crud':
+                handlers.push(new CRUDHandler({
+                    crudCallback: (cruddata) => {
+                        console.log(cruddata);
+                    }
+                }))
+        }
+    }
+    return handlers;
+}
+
+function processURL(): void {
+    const URL = args._[0];
+    const client = new ApiClient(null);
+    let handlers = createHandlers(client);
+    client.fetch(URL, handlers);
+}
+
+processURL();
